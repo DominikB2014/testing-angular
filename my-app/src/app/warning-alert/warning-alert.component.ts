192c22ee6de049d9891d3154bbda85904a23034c
Dominik Buszowiecki <buszowid@mcmaster.ca>
2020-03-21 15:27:45 -0400

    A1 Completed

--- my-app/src/app/warning-alert/warning-alert.component.ts
+++ my-app/src/app/warning-alert/warning-alert.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./warning-alert.component.css']
 })
 export class WarningAlertComponent implements OnInit {
+  message = 'WARNING ALERT!'
 
   constructor() { }
 
