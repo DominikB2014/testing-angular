0a62cb93fb162aaf228b697202d51314563c57dc
Dominik Buszowiecki <buszowid@mcmaster.ca>
2020-03-27 23:27:25 -0400

    Dynamic generation of recipe-items in recipe-list component

--- my-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://i2.wp.com/www.primaverakitchen.com/wp-content/uploads/2019/03/Garlic-Butter-Baked-Chicken-Breast-Primavera-Kitchen-2-500x375.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://i2.wp.com/www.primaverakitchen.com/wp-content/uploads/2019/03/Garlic-Butter-Baked-Chicken-Breast-Primavera-Kitchen-2-500x375.jpg')
+  ];
 
   constructor() { }
 
